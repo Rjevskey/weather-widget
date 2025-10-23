de0bc5114fbbb11cca2098d6d288a7d79d3c9aa6
Rjevskey <yamschikov.vania@yandex.ru>
2025-10-23 11:07:28 +0300

    work commit 23.10.25

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import MyButton from "./components/MyButton.vue";
+import IconComponent from "./assets/icon/icons-location.vue";
 import Stat from "./components/Stat.vue";
 </script>
 
 <template>
     <main class="main">
-        <Stat />
-        <MyButton> Сохранить </MyButton>
+        <Stat label="Влажность" stat="0%" />
+        <Stat label="Осадки" stat="0%" />
+        <MyButton>
+            <IconComponent></IconComponent>
+            Сохранить
+        </MyButton>
     </main>
 </template>
 
---
+++ src/assets/icon/icons-location.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <svg
+        width="15"
+        height="17"
+        viewBox="0 0 25 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M12.5 0C5.6 0 0 5.6 0 12.5C0 21.875 12.5 30 12.5 30C12.5 30 25 21.875 25 12.5C25 5.6 19.4 0 12.5 0ZM12.5 17.5C9.4625 17.5 7 15.0375 7 12.5C7 9.9625 9.4625 7.5 12.5 7.5C15.0375 7.5 17.5 9.9625 17.5 12.5C17.5 15.0375 15.0375 17.5 12.5 17.5Z"
+            fill="none"
+            stroke="#FFFFFF"
+            stroke-width="2.5"
+        />
+    </svg>
+</template>
